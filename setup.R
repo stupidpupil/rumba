47dd304cbf60fcba41959fea6b6d4860a82d24dd
Adam Watkins <adam@stupidpupil.co.uk>
2021-09-01 18:36:21 +0100

    Add new packages

--- setup.R
+++ setup.R
@@ -1,4 +1,4 @@
-for (package in c("tidyverse", "shiny", "shinydashboard", "R6", "xml2" ,"yaml")) {
+for (package in c("tidyverse", "shiny", "shinydashboard", "R6", "xml2" ,"yaml", "pingr", "shinyloadtest")) {
   if (!package %in% installed.packages()) {
     install.packages(package)
   }
